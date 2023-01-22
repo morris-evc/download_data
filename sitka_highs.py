5745adc4101165ada73e80ee3fcb8ac4a5a96edd
morris-evc <evc.morris@gmail.com>
2023-01-22 09:59:26 -0500

    Import datetime Created 2 empty lists to store dates and high temps, convert data containing date info to a datetime object, append it to dates, pass dates and high temps values to plot(). Use fig,auto_xdate () to draw labels diagonally to prevent overlap.

--- sitka_highs.py
+++ sitka_highs.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import csv
 
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 path = Path('weather_data/sitka_weather_7_2021_simple.csv')
 lines = path.read_text().splitlines()
@@ -9,20 +10,23 @@ lines = path.read_text().splitlines()
 reader = csv.reader(lines)
 header_row = next(reader)
 
-# Extract high temperatures.
-highs = []
+# Extract dates and high temperatures.
+dates, highs = [], []
 for row in reader:
+    current_date = datetime.strptime(row[2], '%Y-%m-%d')
     high = int(row[4])
+    dates.append(current_date)
     highs.append(high)
 
 # Plot the high temperatures.
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(highs, color ='red')
+ax.plot(dates, highs, color ='red')
 
 # Format plot.
 ax.set_title("Daily High Temperatures, July 2021, fontsize=24")
 ax.set_xlabel('', fontsize=16)
+fig.autofmt_xdate()
 ax.set_ylabel("Temperature (F)", fontsize=16)
 ax.tick_params(labelsize=16)
 
